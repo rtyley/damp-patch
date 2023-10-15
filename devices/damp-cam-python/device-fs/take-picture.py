a587ccd8fb82de5a23e9460793536e9a21943549
Roberto Tyley <roberto.tyley@gmail.com>
2023-10-15 14:48:41 +0100

    Report upload status

--- devices/damp-cam-python/device-fs/take-picture.py
+++ devices/damp-cam-python/device-fs/take-picture.py
@@ -24,17 +24,7 @@ DAC.set_DAC_outrange(OUTPUT_RANGE_10V)
 
 lens_pos = 0.8
 
-now = datetime.datetime.now().replace(microsecond=0)
-ts = now.isoformat().replace(':', '-')
-print(ts)
-
 exposure_time = 8500
-
-fileName = f"{ts}.exposure-{exposure_time}.jpg"
-tmpDir = f"/tmp/damp-cam-tmp/{now.strftime('%Y-%m-%d/%H')}"
-# with tempfile.TemporaryDirectory() as tmpDir:
-print(f'Folder directory: {tmpDir}')
-Path(tmpDir).mkdir(parents=True, exist_ok=True)
 
 
 def set_light(on: bool):
@@ -46,9 +36,9 @@ def upload_image(filepath: str):
     print(f'Uploading {filePath}')
     with open(filePath, 'rb') as f:
         r = requests.post(
-            f'https://www.teleport.io/api/v1/frame-set/fetebzuxkyjl?apikey=${teleport_api_key}',
+            f'https://www.teleport.io/api/v1/frame-set/fetebzuxkyjl?apikey={teleport_api_key}',
             data=f)
-        print(f'Upload complete! {r.status_code}')
+        print(f'Upload complete! {r.status_code} - {"GOOD" if r.status_code == 200 else "Malfunction?"}')
 
 
 with Picamera2() as picam:
@@ -59,6 +49,14 @@ with Picamera2() as picam:
          "AnalogueGain": 1.0})
     picam.start()
 
+    now = datetime.datetime.now().replace(microsecond=0)
+    ts = now.isoformat().replace(':', '-')
+    print(ts)
+    fileName = f"{ts}.exposure-{exposure_time}.jpg"
+    tmpDir = f"/tmp/damp-cam-tmp/{now.strftime('%Y-%m-%d/%H')}"
+    # with tempfile.TemporaryDirectory() as tmpDir:
+    print(f'Folder directory: {tmpDir}')
+    Path(tmpDir).mkdir(parents=True, exist_ok=True)
     filePath = os.path.join(tmpDir, fileName)
 
     set_light(on=True)
